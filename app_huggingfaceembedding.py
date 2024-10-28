6e3ae19027938d979c266fedb8f8a23ee5acd659
Faisal Mehmood Awan <96324075+FaisalAwa@users.noreply.github.com>
2024-10-28 11:11:16 +0500

    Add files via upload

---
+++ app_huggingfaceembedding.py
@@ -0,0 +1,84 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+import openai
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Load environment variables for API keys
+os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
+os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+# Set up Huggingface embedding model if no OpenAI key is available
+os.environ['HF_TOKEN'] = os.getenv("HF_TOKEN")
+from langchain_huggingface import HuggingFaceEmbeddings
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+# Define LLM model
+llm = ChatGroq(groq_api_key=groq_api_key, model_name="Llama3-8b-8192")
+
+# Prompt template for the Chat model
+prompt = ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate response based on the question.
+    <context>
+    {context}
+    <context>
+    Question: {input}
+    """
+)
+
+def create_vector_embedding():
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+        st.session_state.loader = PyPDFDirectoryLoader("research_papers")  # Data Ingestion step
+        st.session_state.docs = st.session_state.loader.load()  # Document Loading
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)
+
+st.title("RAG Document Q&A With Groq And Llama3")
+
+# Add links to PDF files and GitHub repository
+st.write("### Resources")
+st.write("[PDF Document 1](<https://drive.google.com/file/d/1JoOOO8qKnrv_0sthbvDcYOp1Q7hrRUG6/view?usp=sharing>)")
+st.write("[PDF Document 2](<https://drive.google.com/file/d/1wiW_TjEAdhMXaNCBrh9_HKlct7EnHnPW/view?usp=sharing>)")
+st.write("[GitHub Repository](<https://github.com/FaisalAwa/RAG_QA/>)")
+
+# User input for querying the document
+user_prompt = st.text_input("Enter your query from the above mentioned research paper, open the link , and ask the questions from the avaiable pdf")
+
+if st.button("Document Embedding"):
+    create_vector_embedding()
+    st.write("Vector Database is ready")
+
+import time
+
+if user_prompt:
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    retriever = st.session_state.vectors.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+    start = time.process_time()
+    response = retrieval_chain.invoke({'input': user_prompt})
+    print(f"Response time: {time.process_time() - start}")
+
+    # Display response
+    st.write(response['answer'])
+
+    # Similarity search results in an expander
+    with st.expander("Document similarity Search"):
+        for i, doc in enumerate(response['context']):
+            st.write(doc.page_content)
+            st.write('------------------------')
